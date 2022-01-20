b877f5d7727e743936b139bcfebb7933246a0ecd
Zachary Fuller <zachary.fuller.dev@gmail.com>
2022-01-20 00:29:02 -0500

    Add back old vuex store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,11 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    regions: [2],
+    showNames: null,
+    highScoreName: null,
   },
   mutations: {
+    initiliazeDataStore() {
+      this.state.regions = JSON.parse(localStorage.getItem("regions"));
+      this.state.showNames = JSON.parse(localStorage.getItem("showNames"));
+      this.state.highScoreName = JSON.parse(localStorage.getItem("highScoreName"));
+    },
+    updateRegions(state, payload) {
+      this.state.regions = payload;
+    },
+    updateShowNames(state, payload) {
+      this.state.showNames = payload;
+    }
   },
-  actions: {
-  },
-  modules: {
+  getters: {
+    getActiveRegions: state => {
+      return state.regions
+        .filter(region => region.isActive)
+        .map(region => region.name);
+    }
   }
 })
