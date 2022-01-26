5f8cff6eafcac99bd759d4fda675e3831f2dbd30
Zachary Fuller <zachary.fuller.dev@gmail.com>
2022-01-26 17:44:41 -0500

    Make changes for server hosting

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import { router } from "./routes/scoreRoutes.js";
 dotenv.config({ path: pwd + "/.env" });
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
--- server/config.js
+++ server/config.js
@@ -4,10 +4,18 @@ import { pwd } from "./directoryHelper.js";
 
 dotenv.config({ path: pwd + "/.env" });
 
+// For running on local machine
+// export const dbPool = new pg.Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 export const dbPool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
